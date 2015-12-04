57263fd6675a3bbc3e8b9cc7936fac89b5e06c4f
Eseaflower <eseaflower@hotmail.com>
2015-12-04 16:57:39 +0100

    Indexing

--- rnn_test.py
+++ rnn_test.py
@@ -6,13 +6,14 @@ from collections import OrderedDict
 
 
 class model(object):       
-     def __init__(self, nh, nc, ne, de, cs): 
+     def __init__(self, nh, nc, ne, de, cs, bs): 
          ''' 
          nh :: dimension of the hidden layer 
          nc :: number of classes 
          ne :: number of word embeddings in the vocabulary 
          de :: dimension of the word embeddings 
          cs :: word window context size  
+         bs :: batch size (number of samples)
          ''' 
          # parameters of the model 
          self.emb = theano.shared(0.2 * np.random.uniform(-1.0, 1.0, (ne+1, de)).astype(theano.config.floatX)) # add one for PADDING at the end 
@@ -21,16 +22,15 @@ class model(object):
          self.W   = theano.shared(0.2 * np.random.uniform(-1.0, 1.0,(nh, nc)).astype(theano.config.floatX)) 
          self.bh  = theano.shared(np.zeros(nh, dtype=theano.config.floatX)) 
          self.b   = theano.shared(np.zeros(nc, dtype=theano.config.floatX)) 
-         self.h0  = theano.shared(np.zeros(nh, dtype=theano.config.floatX)) 
+         self.h0  = theano.shared(np.zeros((bs,nh), dtype=theano.config.floatX)) 
  
  
          # bundle 
          self.params = [ self.emb, self.Wx, self.Wh, self.W, self.bh, self.b, self.h0 ] 
          self.names  = ['embeddings', 'Wx', 'Wh', 'W', 'bh', 'b', 'h0'] 
-         idxs = T.imatrix() # as many columns as context window size/lines as words in the sentence 
-         x = self.emb[idxs].reshape((idxs.shape[0], de*cs)) 
-         y    = T.iscalar('y') # label 
-         y_sentence = T.ivector('y_sentence')
+         idxs = T.itensor3() # time->samples->features as many columns as context window size/lines as words in the sentence 
+         x = self.emb[idxs].reshape((idxs.shape[0], bs, de*cs))          
+         y_sentence = T.imatrix('y_sentence')
  
          def recurrence(x_t, h_tm1): 
              h_t = T.nnet.sigmoid(T.dot(x_t, self.Wx) + T.dot(h_tm1, self.Wh) + self.bh) 
@@ -42,23 +42,24 @@ class model(object):
  
  
          p_y_given_x_lastword = s[-1,0,:] 
-         p_y_given_x_sentence = s[:,0,:] 
+         #p_y_given_x_sentence = s[:,0,:] 
+         #p_y_given_x_sentence = s.reshape((x.shape[0]* bs, nc)) 
+         p_y_given_x_sentence = s
+         #y_pred = T.argmax(p_y_given_x_sentence, axis=1) 
          y_pred = T.argmax(p_y_given_x_sentence, axis=1) 
  
  
          # cost and gradients and learning rate 
          lr = T.scalar('lr') 
 
-         sentence_nll = -T.mean(T.log(p_y_given_x_sentence)[T.arange(x.shape[0]), y_sentence])
+         sentence_nll = -T.mean(T.log(p_y_given_x_sentence)[T.arange(x.shape[0]).dimshuffle(0,'x'),T.arange(bs).dimshuffle('x', 0), y_sentence])
+                  
+         #sentence_nll = -T.mean(T.log(p_y_given_x_sentence)[T.arange(x.shape[0]*bs), y_sentence.flatten()])
          sentence_error = T.sum(T.neq(y_pred, y_sentence))
          sentence_gradients = T.grad(sentence_nll, self.params)
          sentence_updates = OrderedDict((p, p - lr*g) for p, g in zip(self.params, sentence_gradients))
 
 
-         nll = -T.log(p_y_given_x_lastword)[y] 
-         gradients = T.grad( nll, self.params ) 
-         updates = OrderedDict(( p, p-lr*g ) for p, g in zip( self.params , gradients)) 
-          
          # theano functions 
          self.classify = theano.function(inputs=[idxs], outputs=y_pred) 
 
@@ -68,10 +69,6 @@ class model(object):
  
          self.errors = theano.function(inputs=[idxs, y_sentence], outputs=sentence_error)             
 
-         self.train = theano.function( inputs  = [idxs, y, lr], 
-                                       outputs = nll, 
-                                       updates = updates ) 
- 
  
          self.normalize = theano.function( inputs = [], 
                           updates = {self.emb: self.emb/T.sqrt((self.emb**2).sum(axis=1)).dimshuffle(0,'x')})  
@@ -81,16 +78,16 @@ def loadData():
     dataPath = r"../Data/atis.pkl"
 
     with open(dataPath, "rb") as f:
-        train, test, dicts = pickle.load(f, encoding="bytes",)
+        train, test, dicts = pickle.load(f, encoding="latin1")
 
 
     converted = dict()
     for ds in dicts.items():
-        dict_name = ds[0].decode("utf-8")    
+        dict_name = ds[0]#.decode("utf-8")    
         inner_dict = dict()    
         converted[dict_name] = inner_dict
         for v in ds[1].items():
-             key = v[0].decode("utf-8")
+             key = v[0]#.decode("utf-8")
              inner_dict[key] = v[1]
 
     return converted["words2idx"], converted["labels2idx"], converted["tables2idx"], train, test
@@ -131,17 +128,33 @@ rnn = model(    nh = 100,
                      nc = nclasses, 
                      ne = vocsize, 
                      de = 100, 
-                     cs = win) 
+                     cs = win,
+                     bs = bs) 
 
 for e in range(50):     
     print("Epoch {0}".format(e))
-    for i in range(nsentences): 
-        if i % 10 == 0:
-            print("{0}%".format(i*100/nsentences))
-        cwords = contextwin(train_lex[i], win) 
-        #words  = map(lambda x: np.asarray(x).astype('int32'), minibatch(cwords, bs)) 
-        labels = train_y[i]
-        rnn.train_sentence(cwords, labels, lr)
+    batchSize = 5
+    nBatches = nsentences // batchSize
+    #for i in range(nsentences): 
+    for b in range(nBatches): 
+        
+        #if i % 100 == 0:
+        #    print("{0}%".format(i*100/nsentences))
+        s = 0
+        r = []
+        for i in range(b*batchSize, (b+1)*batchSize):
+            cw = contextwin(train_lex[i], win)
+            s = np.max([s, len(cw)])
+            r.append(cw)
+        
+        input = np.zeros((s, bs, win), dtype='int32')
+                                
+        #cwords = [contextwin(train_lex[i], win) for i in range(b*batchSize, (b+1)*batchSize)] 
+        #words  = map(lambda x: np.asarray(x).astype('int32'), minibatch(cwords, bs))         
+        labels = [train_y[i] for i in range(b*batchSize, (b+1)*batchSize)]
+        targets = np.zeros((s, bs), dtype='int32')
+        #rnn.train_sentence(cwords, labels, lr)
+        rnn.train_sentence(input, targets, lr)
         rnn.normalize()
         #for word_batch , label_last_word in zip(words, labels): 
             #rnn.train(word_batch, label_last_word, lr) 
@@ -151,6 +164,6 @@ for e in range(50):
     for i in range(len(test_lex)):
         cwords = contextwin(test_lex[i], win)
         labels = test_y[i]
-        cum_errors += rnn.errors(cwords, labels)
+        #cum_errors += rnn.errors(cwords, labels)
 
     print("Test errors {0}".format(cum_errors))
